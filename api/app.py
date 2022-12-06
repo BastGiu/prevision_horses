f8dc809bba38ac56e4fd00c7d440f6188a8bb576
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-06 16:56:56 +0100

    Test5

--- api/app.py
+++ api/app.py
@@ -2,9 +2,7 @@
 
 import pandas as pd
 import streamlit as st
-from fast import predict_winner
 import streamlit as st
-import requests
 from PIL import Image
 
 url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/2022-12-06_df_streamlit.csv'
@@ -13,10 +11,18 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-unicorn = st.select_slider("Displayed values:", ["🏇🏼", "🦄"])
+unicorn = st.select_slider("Displayed values:", ["🦄", "🏇🏼"])
 image = Image.open('data/images/chevaux.jpg')
 st.image(image, caption='Chevals', width=650)
 
+tog.st_toggle_switch(label="Magic button",
+                    key="Key1",
+                    default_value="🏇🏼",
+                    label_after = "🦄",
+                    inactive_color = '#D3D3D3',
+                    active_color="#11567f",
+                    track_color="#29B5E8"
+                    )
 if unicorn:
     st.write('''# Pon.A.I 🦄''')
 else:
@@ -35,7 +41,7 @@ df = display_dataframe(data_url=data_url)
 st.dataframe(df)
 
 image2 = Image.open('data/images/people.png')
-st.image(image2, caption='Nous 4 dans quelques jours', width=650)
+st.image(image2, caption='Nous 4 dans quelques jours', width=625)
 
 hide_menu_style = """
         <style>
