fc38c7a8579c5538baf613c78041f3653eac1397
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-08 11:42:55 +0100

    display données d'hier

--- api/pages/02_Performances.py
+++ api/pages/02_Performances.py
@@ -8,7 +8,7 @@ st.markdown("### Données d'hier ")
 data_url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/df_perf_modele_2022-12-03__2022-12-07.csv'
 def display_dataframe(data_url):
     df = pd.read_csv(data_url)
-    df.drop(columns=["jour", "proba pl", "country"], inplace=True)
+    df.drop(columns=["jour", "proba pl"], inplace=True)
     df.heure = df.heure.map(lambda x: str(x)[0:-3])
     df.rename(columns={'hippo': 'Hippodrome', 'prixnom': 'Prix', 'cheval': 'Cheval','numero' : 'Numéro','heure':'Heure course'}, inplace=True)
     return df[["Hippodrome", "Heure course", "Prix", "Cheval", "Numéro"]]
