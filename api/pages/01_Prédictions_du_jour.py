bd7545e435e8ba09f4bdc5e79187a105e8898863
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-08 12:21:50 +0100

    Correct perf pred

--- api/pages/01_Prédictions_du_jour.py
+++ api/pages/01_Prédictions_du_jour.py
@@ -10,19 +10,6 @@ st.set_page_config(
     page_icon='🏇🏼'
     )
 
-url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/2022-12-06_df_streamlit.csv'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-st.sidebar.markdown("# Prédictions du jour")
-st.markdown("<h1 style='text-align: center;'>PonAI 🏇🏼</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center;'>L'AI qui donne des tuyaux </h3>", unsafe_allow_html=True)
-
-# Horses image
-image = Image.open('data/images/chevaux.jpg')
-st.image(image, caption='Chevaux qui galopent', width=650)
 
 data_url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/df_pred_2022-12-08_pr_bastos.csv'
 def display_dataframe(data_url):
