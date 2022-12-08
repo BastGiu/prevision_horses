2eed83086d67ad1e22902494415acb3a4f284c08
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-08 12:25:32 +0100

    Correct perf pred

--- api/pages/01_Prédictions_du_jour.py
+++ api/pages/01_Prédictions_du_jour.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_icon='🏇🏼'
     )
 
-
+st.sidebar.markdown("# Prédictions du jour")
 data_url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/df_pred_2022-12-08_pr_bastos.csv'
 def display_dataframe(data_url):
     df = pd.read_csv(data_url)
