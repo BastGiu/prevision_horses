ec432176bc0e9fe506f778ae3c4debe7644465d5
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-08 12:05:29 +0100

    Correct perf pred

--- api/pages/02_Performances.py
+++ api/pages/02_Performances.py
@@ -4,13 +4,14 @@ import pandas as pd
 st.markdown("# Performances ")
 st.markdown("### Bonnes prédictions du modèle sur la semaine écoulée")
 st.markdown("### Données d'hier ")
+st.sidebar.markdown("# Performances du modèle")
 
 data_url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/df_perf_modele_2022-12-03__2022-12-07.csv'
 def display_dataframe(data_url):
     df = pd.read_csv(data_url)
-    df.drop(columns=["cl", "pl", "id", "coteprob","cotedirect","predi_placé", "bonne_pred", "numcourse"], inplace=True)
-    # df.heure = df.heure.map(lambda x: str(x)[0:-3])
-    df.rename(columns={'jour' : 'Date','hippo': 'Hippodrome', 'prixnom': 'Prix', 'cheval': 'Cheval','numero' : 'Numéro','heure':'Heure course'}, inplace=True)
+    df.drop(columns=["id", "coteprob","cotedirect","predi_placé", "bonne_pred", "numcourse", "proba pl"], inplace=True)
+    df.cl.astype(int)
+    df.rename(columns={'cl':'Classement', 'jour' : 'Date','hippo': 'Hippodrome', 'prixnom': 'Prix', 'cheval': 'Cheval','numero' : 'Numéro','heure':'Heure course'}, inplace=True)
     return df
 df = display_dataframe(data_url=data_url)
 st.dataframe(df)
