6c3b483e8e0059e864c9ee83a97188e0da0ab0eb
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-08 10:18:57 +0100

    REfined jk

--- api/app.py
+++ api/app.py
@@ -1,5 +1,15 @@
 # Contents of ~/my_app/streamlit_app.py
+# -*- coding: utf-8 -*-
+
+import pandas as pd
 import streamlit as st
+from PIL import Image
+import streamlit as st
+
+st.set_page_config(
+    page_title='ponAI',
+    page_icon='🏇🏼')
+
 
 def main_page():
     st.markdown("# PonAI 🏇🏼")
@@ -17,16 +27,6 @@ page_names_to_funcs = {
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
 
-# -*- coding: utf-8 -*-
-
-import pandas as pd
-import streamlit as st
-from PIL import Image
-import streamlit as st
-
-st.set_page_config(
-    page_title='ponAI',
-    page_icon='🏇🏼')
 
 url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/2022-12-06_df_streamlit.csv'
 
