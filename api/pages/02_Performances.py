0da4d77510e97cec5afde4a6f178909e40a6c04e
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-08 10:58:34 +0100

    Ca bug3

--- api/pages/02_Performances.py
+++ api/pages/02_Performances.py
@@ -1,7 +1,1 @@
 import streamlit as st
-
-def performances():
-    st.markdown("# Bonnes prédictions de la semaine")
-    st.sidebar.markdown("# Bonnes prédictions")
-    st.markdown("# Performances du modèle sur les cinq derniers jours")
-    st.sidebar.markdown("#  Performances du modèle")
