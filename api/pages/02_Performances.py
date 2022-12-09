60ff4148e253ad1f28e995f2d1cc452e2c146ba1
Bastien Giudicelli <giudicellibastien@gmail.com>
2022-12-09 11:38:49 +0100

    PErformances updated

--- api/pages/02_Performances.py
+++ api/pages/02_Performances.py
@@ -8,7 +8,7 @@ st.markdown("# Performances ")
 st.markdown("### Bonnes prédictions récentes ")
 st.sidebar.markdown("# Performances du modèle")
 
-data_url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/df_perf_modele_2022-12-03__2022-12-07.csv'
+data_url = 'https://raw.githubusercontent.com/BastGiu/prevision_horses/master/data/df_perf_modele_2022-12-03__2022-12-08.csv'
 def display_dataframe(data_url):
     df = pd.read_csv(data_url)
     df.drop(columns=["pl", "id", "coteprob","cotedirect","predi_placé", "bonne_pred", "numcourse", "proba pl"], inplace=True)
